38c203b026490c79ecaa2b91133128f59638ad4e
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2007-12-02 22:18:59 +0000

    correct few bugs: Start removing pk on multiple column

---
+++ minuteKernel/src/net/sf/minuteProject/utils/WebUtils.java
@@ -0,0 +1,39 @@
+package net.sf.minuteProject.utils;
+
+import net.sf.minuteProject.configuration.bean.Model;
+
+public class WebUtils {
+	
+	public static final String SITEMESHMAINLAYOUT_TEMPLATE = "SitemeshMainLayout";
+	public static final String GEN_CRUD_SERVLET_TEMPLATE = "GenCRUDServlet";
+	public static final String BSLASPRINGCONFIG_TEMPLATE = "BslaSpringConfig";
+	
+	public String getDefaultRootURLBeginner(Model model) {
+		return URLUtils.getDefaultRootURLBeginner();
+	}
+	
+	public static String getSitemeshMainDecorator(Model model) {
+		return CommonUtils.getTemplateArtifactName (model, SITEMESHMAINLAYOUT_TEMPLATE);
+	}
+	
+	public static String getSitemeshMainDecoratorDir(Model model) {
+		return CommonUtils.getTemplateArtifactDirName (model, SITEMESHMAINLAYOUT_TEMPLATE);
+	}
+	
+	public static String getProjectDefaultServletName(Model model) {
+		return CommonUtils.getTemplateArtifactName (model, GEN_CRUD_SERVLET_TEMPLATE);		
+	}
+	
+	public static String getProjectDefaultServletClasspath(Model model) {
+		return CommonUtils.getArtifactFullClasspath (model, GEN_CRUD_SERVLET_TEMPLATE);				
+	}
+	
+	public static String getDefaultHomePageContent(Model model) {
+		return CommonUtils.getArtifactRelativePathDirAndFullName (model, GEN_CRUD_SERVLET_TEMPLATE);						
+	}
+	
+	public static String getSpringConfigMainInRootClassPath(Model model) {
+		return CommonUtils.getArtifactRelativePathDirAndFullName (model, BSLASPRINGCONFIG_TEMPLATE);								
+	}
+	
+}
