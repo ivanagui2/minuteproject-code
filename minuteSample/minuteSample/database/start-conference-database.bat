58e6533c7cf792e99cf5c1c7a35b602c7af7bf03
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2008-01-14 22:12:30 +0000

    first sample (conference db)

---
+++ minuteSample/minuteSample/database/start-conference-database.bat
@@ -0,0 +1,3 @@
+
+java -classpath hsql/hsqldb-1.7.3.jar org.hsqldb.Server -database.0 ./hsql/conference -dbname.0 conference -port 9001 -silent true -trace false
+@rem java -classpath hsql/hsqldb-1.7.3.jar org.hsqldb.Server -database.0 ./hsql/openreports -dbname.0 openreports -database.1 ./hsql/sample -dbname.1 sample -port 9001 -silent true -trace false
