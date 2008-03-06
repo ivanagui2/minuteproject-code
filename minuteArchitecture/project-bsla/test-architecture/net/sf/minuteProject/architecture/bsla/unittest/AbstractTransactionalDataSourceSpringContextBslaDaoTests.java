c3da2f8c47f0d7e1c05c101a4f5c60d06c2bab01
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2008-03-06 21:58:25 +0000

    bsla test structure

---
+++ minuteArchitecture/project-bsla/test-architecture/net/sf/minuteProject/architecture/bsla/unittest/AbstractTransactionalDataSourceSpringContextBslaDaoTests.java
@@ -0,0 +1,12 @@
+package net.sf.minuteProject.architecture.bsla.unittest;
+
+public abstract class AbstractTransactionalDataSourceSpringContextBslaDaoTests 
+   extends AbstractTransactionalDataSourceSpringContextBslaTests {
+
+	@Override
+	public void onSetUpInBslaTestTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
