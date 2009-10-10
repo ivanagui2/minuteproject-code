1e4501f4c4b6cab1097dfdf2ab3632dfd711520a
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2009-10-10 21:23:42 +0000

    mp-fitnesse

---
+++ trunk/minuteArchitecture/project-bsla/src-architecture-mp-fitnesse/net/sf/minuteproject/fitnesse/fixture/DbInsertFixture.java
@@ -0,0 +1,43 @@
+package net.sf.minuteproject.fitnesse.fixture;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.minuteproject.utils.database.DatabaseUtils;
+import net.sf.minuteproject.utils.query.QueryUtils;
+import fit.ColumnFixture;
+
+public abstract class DbInsertFixture extends ColumnFixture{
+
+	public String insert() {
+		
+		String query = QueryUtils.buildInsertStatement(getTable(), getColumnIndex(), getColumnValue());
+		return insert(query);
+	}
+	
+	protected String insert(String query) {
+		Connection connection;
+		try {
+			connection = DatabaseUtils.getConnection();
+			if (connection ==null)
+				System.out.println("connection is null");
+			Statement st = connection.createStatement();
+			st.executeUpdate(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return (e.getMessage());
+		} 
+		return "OK";
+	}
+	
+	protected abstract String getTable();
+	
+	protected abstract Map<Integer, String> getColumnIndex () ;
+	
+	protected abstract Map<String, String> getColumnValue () ;
+
+}
