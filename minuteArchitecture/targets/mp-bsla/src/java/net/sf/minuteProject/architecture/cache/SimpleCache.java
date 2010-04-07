a06bb08329a6a61293489d2ec9272d7501b846e1
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2010-04-07 14:26:54 +0000

    simple clean

--- minuteArchitecture/targets/mp-bsla/src/java/net/sf/minuteProject/architecture/cache/SimpleCache.java
+++ minuteArchitecture/targets/mp-bsla/src/java/net/sf/minuteProject/architecture/cache/SimpleCache.java
@@ -7,7 +7,9 @@ import java.util.Hashtable;
 public class SimpleCache implements Cache {
 	
 	private Long delay;
-	private long delayDefault = 1000*60;
+	private final long delayDefault = 1000*60;
+	private final int  maxKeyDefault = 100;
+	private Integer  maxKeys;
 	
 	private static Hashtable table = new Hashtable();
 	private static Hashtable timeTable = new Hashtable();
@@ -47,21 +49,28 @@ public class SimpleCache implements Cache {
 	 * @see net.sf.minuteProject.architecture.cache.Cache2#put(java.lang.String, java.lang.Object)
 	 */
     public void put(String key, Object o) {
-    	table.put(key, o);
-    	timeTable.put(key, new Date());
+    	if (timeTable.size()>getMaxKeys()) //to limit the size of the table (it is no the best algo ;))
+    		flush();
+    	putEffective(key, o);
     }
     
+    private void putEffective(String key, Object o) {
+    	timeTable.put(key, new Date());
+    	table.put(key, o);
+    }
     /* (non-Javadoc)
 	 * @see net.sf.minuteProject.architecture.cache.Cache2#remove(java.lang.String)
 	 */
     public void remove(String key) {
-        table.remove(key);
+    	timeTable.remove(key);
+    	table.remove(key); 
     }
     
     /* (non-Javadoc)
 	 * @see net.sf.minuteProject.architecture.cache.Cache2#flush()
 	 */
     public void flush() {
+    	timeTable.clear();
     	table.clear();
     }
 
@@ -78,4 +87,16 @@ public class SimpleCache implements Cache {
 	public void setRefreshDelay(Long delay) {
 		setDelay(delay);
 	}
+
+	public Integer getMaxKeys() {
+		if (maxKeys==null)
+			setMaxKeys(maxKeyDefault);
+		return maxKeys;
+	}
+
+	public void setMaxKeys(Integer maxKeys) {
+		this.maxKeys = maxKeys;
+	}
+	
+	
 }
