15798fc9a188c72302acd880d95c6e7d5e7ea032
chiovari77 <chiovari77@64cedb4e-4427-0410-8520-cb93c3a6829a>
2010-07-01 08:04:41 +0000

    Added a list to result holder for extra information

--- trunk/minuteArchitecture/targets/mp-bsla/src/java/net/sf/minuteProject/architecture/holder/ResultHolder.java
+++ trunk/minuteArchitecture/targets/mp-bsla/src/java/net/sf/minuteProject/architecture/holder/ResultHolder.java
@@ -1,5 +1,8 @@
 package net.sf.minuteProject.architecture.holder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sf.minuteProject.architecture.holder.element.InformationMessage;
 import net.sf.minuteProject.architecture.holder.element.ValidationError;
 
@@ -8,40 +11,41 @@ public class ResultHolder {
 	private ValidationHolder validationHolder;
 	private InformationHolder informationHolder;
 	private Object object;
+	private List<Object> resultHolderObjectList = new ArrayList<Object>();
 
-	public void complement (ResultHolder resultHolder) {
+	public void complement(ResultHolder resultHolder) {
 		for (ValidationError validationError : resultHolder.getValidationHolder().getValidationErrors()) {
-//			this.getValidationHolder().add(validationError);
+			// this.getValidationHolder().add(validationError);
 			addValidationError(validationError);
 		}
 		for (InformationMessage informationMessage : resultHolder.getInformationHolder().getMessages()) {
 			addInformationMessage(informationMessage);
-		}		
+		}
 	}
-	
-	public void addValidationError (ValidationError validationError) {
+
+	public void addValidationError(ValidationError validationError) {
 		getValidationHolder().add(validationError);
 	}
-	
-	public void addInformationMessage (InformationMessage informationMessage) {
+
+	public void addInformationMessage(InformationMessage informationMessage) {
 		getInformationHolder().add(informationMessage);
 	}
-	
+
 	public Object getObject() {
 		return object;
 	}
-	
+
 	public ResultHolder putObject(Object object) {
 		this.setObject(object);
 		return this;
 	}
-	
+
 	public void setObject(Object object) {
 		this.object = object;
 	}
 
 	public ValidationHolder getValidationHolder() {
-		if (validationHolder==null)
+		if (validationHolder == null)
 			validationHolder = new ValidationHolder();
 		return validationHolder;
 	}
@@ -51,15 +55,21 @@ public class ResultHolder {
 	}
 
 	public InformationHolder getInformationHolder() {
-		if (informationHolder==null)
-			informationHolder = new InformationHolder();		
+		if (informationHolder == null)
+			informationHolder = new InformationHolder();
 		return informationHolder;
 	}
 
 	public void setInformationHolder(InformationHolder informationHolder) {
 		this.informationHolder = informationHolder;
 	}
-	
-	
-	
+
+	public List<Object> getResultHolderObjectList() {
+		return resultHolderObjectList;
+	}
+
+	public void setResultHolderObjectList(List<Object> resultHolderObjectList) {
+		this.resultHolderObjectList = resultHolderObjectList;
+	}
+
 }
