1aa14e028e1b65aa4f37b747209b7c164032c9b0
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2010-09-12 15:42:36 +0000

    porting to linux

--- minuteDemo/demo/config/demo-model-generation.sh
+++ minuteDemo/demo/config/demo-model-generation.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-PROGDIR=$(cd `dirname $0`/../../..;pwd)  
-echo progdir = $PROGDIR
+CURRENTDIR=$(cd `dirname $0`;pwd)
+PROGDIR=$(cd `dirname $0`/../..;pwd)  
+echo CURRENTDIR = $CURRENTDIR
 
-MP_LIB=$PROGDIR/minuteKernel/lib
-#MP_LIB=$PROGDIR/application/lib
+#MP_LIB=$PROGDIR/minuteKernel/lib
+#MP_CLASSES=$PROGDIR/minuteKernel/bin
+MP_LIB=$PROGDIR/application/lib
 LOCALCLASSPATH=
 for i in `ls $MP_LIB/*.jar` `ls $MP_LIB/**/*.jar`
 do
   LOCALCLASSPATH=${LOCALCLASSPATH}:"${i}"
 done
+#LOCALCLASSPATH=${LOCALCLASSPATH}:$MP_CLASSES:$CURRENTDIR
+LOCALCLASSPATH=${LOCALCLASSPATH}
 
-"$JAVA_HOME/bin/java" -classpath `cygpath -wp $LOCALCLASSPATH` net.sf.minuteProject.application.ModelViewGenerator $1
+"$JAVA_HOME/bin/java" -cp $LOCALCLASSPATH net.sf.minuteProject.application.ModelViewGenerator $1
 
