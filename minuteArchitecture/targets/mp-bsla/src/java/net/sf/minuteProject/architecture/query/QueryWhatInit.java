c87ccfecf4e2fc594b9222867b5d5e60e2509695
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2010-09-16 13:38:29 +0000

    various for 0.5

--- minuteArchitecture/targets/mp-bsla/src/java/net/sf/minuteProject/architecture/query/QueryWhatInit.java
+++ minuteArchitecture/targets/mp-bsla/src/java/net/sf/minuteProject/architecture/query/QueryWhatInit.java
@@ -3,4 +3,13 @@ package net.sf.minuteProject.architecture.query;
 public interface QueryWhatInit {
 
 	public String getWhatInit(boolean isComma);
+	
+	public boolean isToSeparateInit();
+	
+	public boolean isProjectionQuery();
+	
+	public String getWhatAlias();
+	
+	public String getWhatProperty();
+	
 }
