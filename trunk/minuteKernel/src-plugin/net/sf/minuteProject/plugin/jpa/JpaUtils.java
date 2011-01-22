f5b4b929f5679606e1226e4910e4f10968614292
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2011-01-22 15:16:27 +0000

    jpa2 metamodel

---
+++ trunk/minuteKernel/src-plugin/net/sf/minuteProject/plugin/jpa/JpaUtils.java
@@ -0,0 +1,10 @@
+package net.sf.minuteProject.plugin.jpa;
+
+import net.sf.minuteProject.configuration.bean.model.data.Table;
+
+public class JpaUtils {
+
+	public static String getMetamodelVolatileObjects (Table table) {
+		return "";
+	}
+}
