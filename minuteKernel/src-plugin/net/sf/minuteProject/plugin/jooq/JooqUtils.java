b18a3c78cea69900f79e18167417b02426e4412d
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2012-01-09 16:07:52 +0000

    jooq config+plugin

--- minuteKernel/src-plugin/net/sf/minuteProject/plugin/jooq/JooqUtils.java
+++ minuteKernel/src-plugin/net/sf/minuteProject/plugin/jooq/JooqUtils.java
@@ -1,9 +1,28 @@
 package net.sf.minuteProject.plugin.jooq;
 
+import net.sf.minuteProject.configuration.bean.model.data.Column;
+import net.sf.minuteProject.configuration.bean.model.data.Table;
+import net.sf.minuteProject.utils.ColumnUtils;
+import net.sf.minuteProject.utils.ConvertUtils;
+import net.sf.minuteProject.utils.TableUtils;
+
 public class JooqUtils {
 
 	public static String getDatabaseJavaPackage (String database) {
 		if ("MYSQL".equals(database)) return "org.jooq.util.mysql.MySQLDatabase";
 		return "to implement";
+	}
+	
+	public static String getEntitySuperClass (Table table) {
+		if (TableUtils.isView(table)) return "to implement";
+		return "org.jooq.impl.UpdatableTableImpl";
+	}
+	
+	public static String getJooqFullType(Column column) {
+		return "rg.jooq.impl.SQLDataType."+column.getType();
+	}
+	
+	public static String getRandomSerialNumber () {
+		return "123456789";//tochange
 	}
 }
