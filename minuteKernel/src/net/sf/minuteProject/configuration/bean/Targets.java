b8437db13a003d2c1fa6d94f271f4f126698aee3
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2014-10-08 19:37:24 +0000

    resources delivery directory

--- minuteKernel/src/net/sf/minuteProject/configuration/bean/Targets.java
+++ minuteKernel/src/net/sf/minuteProject/configuration/bean/Targets.java
@@ -20,7 +20,7 @@ public class Targets extends AbstractConfiguration {
 	private Boolean appendCatalogEntryDirToOutputDirRoot=false;
 	public static final String deliveryPath = "../../template";
 	public static final String developmentPath = "../minuteTemplate/template";
-	public static final String deliveryResourcePath = "../../resource/icon";
+	public static final String deliveryResourcePath = "../../resources/icon";
 	public static final String developmentResourcePath = "../minuteResource/icon";
 	public static final String MP_HOME_RELATIVE_PATH = "/template";
 	private AbstractConfigurationRoot abstractConfigurationRoot;
