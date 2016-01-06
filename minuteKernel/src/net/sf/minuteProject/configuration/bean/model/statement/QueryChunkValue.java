caab02124ff3ee976df3338eb1a677c470e6f7da
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2016-01-06 12:55:15 +0000

    default value in chunk backend

--- minuteKernel/src/net/sf/minuteProject/configuration/bean/model/statement/QueryChunkValue.java
+++ minuteKernel/src/net/sf/minuteProject/configuration/bean/model/statement/QueryChunkValue.java
@@ -2,4 +2,19 @@ package net.sf.minuteProject.configuration.bean.model.statement;
 
 public class QueryChunkValue extends QueryAdapter{
 
+	private boolean isDefault;
+	private QueryChunk queryChunk;
+
+	public boolean isDefault() {
+		return isDefault;
+	}
+
+	public void setIsDefault(boolean isDefault) {
+		this.isDefault = isDefault;
+	}
+
+	public void setQueryChunk(QueryChunk queryChunk) {
+		this.queryChunk = queryChunk;
+	}
+	
 }
