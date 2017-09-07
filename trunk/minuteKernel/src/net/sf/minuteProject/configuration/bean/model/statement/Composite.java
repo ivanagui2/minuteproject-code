3a49cb7e8a2a912fcd250c19f360e872faa45ba9
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2017-09-07 21:10:03 +0000

    composite with input value or implicit input

--- trunk/minuteKernel/src/net/sf/minuteProject/configuration/bean/model/statement/Composite.java
+++ trunk/minuteKernel/src/net/sf/minuteProject/configuration/bean/model/statement/Composite.java
@@ -1,17 +1,24 @@
 package net.sf.minuteProject.configuration.bean.model.statement;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import net.sf.minuteProject.configuration.bean.AbstractConfiguration;
 import net.sf.minuteProject.configuration.bean.Package;
 import net.sf.minuteProject.configuration.bean.Template;
+import net.sf.minuteProject.configuration.bean.model.data.Column;
 import net.sf.minuteProject.configuration.bean.model.data.Table;
 import net.sf.minuteProject.configuration.bean.model.data.constant.Direction;
 
-public class Composite extends AbstractConfiguration{
+public class Composite extends AbstractConfiguration {
 
-	//TODO change Composite to embrace composite pattern
+	// TODO change Composite to embrace composite pattern
 	private Composites composites;
 	private Composite inputComposite, outputComposite;
 	private Direction direction;
@@ -19,9 +26,9 @@ public class Composite extends AbstractConfiguration{
 	private String executionType;
 	private Package pack;
 	private String packageName;
-	
+
 	private List<CompositeQueryElement> queries;
-	
+
 	public void setComposites(Composites composites) {
 		this.composites = composites;
 	}
@@ -35,7 +42,7 @@ public class Composite extends AbstractConfiguration{
 	}
 
 	public List<CompositeQueryElement> getQueries() {
-		if (queries==null)
+		if (queries == null)
 			queries = new ArrayList<CompositeQueryElement>();
 		return queries;
 	}
@@ -48,11 +55,11 @@ public class Composite extends AbstractConfiguration{
 		return composites;
 	}
 
-	public void addQuery (CompositeQueryElement query) {
+	public void addQuery(CompositeQueryElement query) {
 		query.setComposite(this);
 		getQueries().add(query);
 	}
-	
+
 	public Composite getComposite(Direction direction) {
 		if (Direction.IN.equals(direction))
 			return getInputComposite();
@@ -61,18 +68,18 @@ public class Composite extends AbstractConfiguration{
 	}
 
 	public Composite getInputComposite() {
-		if (inputComposite!=null)
+		if (inputComposite != null)
 			return inputComposite;
 		return retrieveComposite(Direction.IN);
 	}
-	
+
 	public Composite getOutputComposite() {
-		if (outputComposite!=null)
+		if (outputComposite != null)
 			return outputComposite;
 		return retrieveComposite(Direction.OUT);
 	}
 
-	private Composite retrieveComposite (Direction dir) {
+	private Composite retrieveComposite(Direction dir) {
 		Composite composite = new Composite();
 		composite.direction = dir;
 		composite.setName(this.name);
@@ -82,7 +89,7 @@ public class Composite extends AbstractConfiguration{
 		for (CompositeQueryElement query : getQueries()) {
 			Query q = query.getQuery();
 			Table table = q.getEntity(dir);
-//			table.setName(q.getName());
+			// table.setName(q.getName());
 			table.setPackage(q.getPackage());
 			composite.getEntities().add(table);
 		}
@@ -90,7 +97,7 @@ public class Composite extends AbstractConfiguration{
 	}
 
 	public List<Table> getEntities() {
-		if (entities==null)
+		if (entities == null)
 			entities = new ArrayList<Table>();
 		return entities;
 	}
@@ -98,14 +105,44 @@ public class Composite extends AbstractConfiguration{
 	public void setEntities(List<Table> entities) {
 		this.entities = entities;
 	}
-	
+
+	public List<Column> getdistinctInputColumn() {
+		//https://stackoverflow.com/questions/23699371/java-8-distinct-by-property
+/*		
+		String [] arr = {"ABQ","ALB","CHI","CUN","PHX","PUJ","BWI"};
+        //final ImmutableList<String> arpts = ImmutableList.of("ABQ","ALB","CHI","CUN","PHX","PUJ","BWI");
+
+		List<String> arpts = Arrays.asList(arr);
+        arpts.stream().filter(distinctByKey(f -> f.substring(0,1))).forEach(s -> System.out.println(s));
+*/
+		return getInputComposite().getEntities()
+			.stream()
+			.flatMap(u -> Arrays.asList(u.getColumns())
+			.stream())
+			.collect(Collectors.toList())
+			.stream()
+			.filter(distinctByKey(p -> p.getName()))
+			.collect(Collectors.toList());
+
+	}
+
+	public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+	    Map<Object,Boolean> seen = new ConcurrentHashMap<>();
+	    return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+	}
+	/*
+	public static <Column> Predicate<Column> distinctColumn(Function<Column, ?> keyExtractor) {
+		return distinctByKey(keyExtractor);
+	}
+	*/
+
 	public String getTechnicalPackage(Template template) {
 		net.sf.minuteProject.configuration.bean.Package p = getPackage();
 		if (p == null)
 			return "ERROR_COMPOSITE_PACKAGE_IS_NULL";
 		return p.getTechnicalPackage(template);
 	}
-	
+
 	public Package getPackage() {
 		return pack;
 	}
@@ -121,9 +158,9 @@ public class Composite extends AbstractConfiguration{
 	public void setDirection(Direction direction) {
 		this.direction = direction;
 	}
-	
-	public boolean isDirection (Direction direction) {
-		return (this.direction==direction);
+
+	public boolean isDirection(Direction direction) {
+		return (this.direction == direction);
 	}
 
 	public String getPackageName() {
@@ -133,5 +170,5 @@ public class Composite extends AbstractConfiguration{
 	public void setPackageName(String packageName) {
 		this.packageName = packageName;
 	}
-	
+
 }
