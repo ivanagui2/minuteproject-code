d122568b86d53ba58266fa7b144984106367e41f
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2019-06-20 15:47:56 +0000

    Kernel config with validation javax

---
+++ trunk/minuteKernel/src/net/sf/minuteProject/configuration/bean/enrichment/validation/ComparisonOperand.java
@@ -0,0 +1,36 @@
+package net.sf.minuteProject.configuration.bean.enrichment.validation;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import lombok.Getter;
+
+@Getter
+public enum ComparisonOperand {
+    EQUALS ("EQUALS", "first-field-equals-second-field"),
+    GREATER_THAN ("GREATER_THAN", "first-field-greater-than-second-field"),
+    GREATER_OR_EQUAL_THAN ("GREATER_OR_EQUAL_THAN", "first-field-greater-or-equals-than-second-field"),
+    LESS_THAN ("LESS_THAN", "first-field-less-than-second-field"),
+    LESS_OR_EQUAL_THAN ("LESS_OR_EQUAL_THAN", "first-less-or-equal-than-second-field"),
+    SECOND_MANDATORY_WHEN_FIRST_FILLED ("SECOND_MANDATORY_WHEN_FIRST_FILLED")
+    ;
+	
+	private final String value, conventionValue;
+	
+	ComparisonOperand(String value) {
+		this(value, null);
+	}
+	
+	ComparisonOperand(String value, String conventionValue) {
+		this.value = value;
+		this.conventionValue = conventionValue;
+	}
+
+	public static Optional<ComparisonOperand> fromConventionValue(String type) {
+		return Arrays.asList(ComparisonOperand.values()).stream()
+			.filter(u -> u.getConventionValue().equals(type))
+			.findFirst()
+			;
+		
+	}
+}
