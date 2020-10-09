92953bbd19cb9423640c23d9f2b4e80a23fef240
florian_adler <florian_adler@64cedb4e-4427-0410-8520-cb93c3a6829a>
2020-10-09 16:14:16 +0000

    MP : kernel : hbs extension + ember utils filename

--- trunk/minuteKernel/src-plugin/net/sf/minuteProject/plugin/ember/EmberUtils.java
+++ trunk/minuteKernel/src-plugin/net/sf/minuteProject/plugin/ember/EmberUtils.java
@@ -1,5 +1,35 @@
 package net.sf.minuteProject.plugin.ember;
 
+import org.apache.commons.lang.StringUtils;
+
+import net.sf.minuteProject.configuration.bean.GeneratorBean;
+import net.sf.minuteProject.configuration.bean.Template;
+import net.sf.minuteProject.utils.FormatUtils;
+
 public class EmberUtils {
+
+	public static String getEmberFileName(Template template, GeneratorBean bean) {
+		if (bean.getName()!=null) {
+			StringBuilder sb = new StringBuilder();
+			if (!StringUtils.isEmpty(template.getFilePrefix())) {
+				sb.append(template.getFilePrefix()+"_");
+			}
+			sb.append(bean.getName());
+			if (!StringUtils.isEmpty(template.getFileSuffix())) {
+				sb.append("_"+template.getFileSuffix());
+			}
+			return sb.toString().replaceAll("_", "-").toLowerCase();
+		}
+		return "name not converted";
+	}
+	
+	public static String getEmberClass(String name) {
+		return FormatUtils.getJavaName(name.replaceAll("-", "_"));
+	}
+	
+	
+	public static String getEmberClassVariable(String name) {
+		return FormatUtils.getJavaNameVariable(name.replaceAll("-", "_"));
+	}
 
 }
